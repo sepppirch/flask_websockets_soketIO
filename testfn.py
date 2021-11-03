ed2c9a69383a7ce18178d0e4183b9636b6050ea9
sebastian <sebastian@OMEN.local>
2021-11-03 10:53:43 +0100

    Update flaskHelloWorld.py, middleman.py, and 520 more files...

---
+++ testfn.py
@@ -0,0 +1,3 @@
+def calculate_it():
+    # do some calculate with the input value
+    return "reeeeeeeee"
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\sebastian\Documents\Unreal Projects\websockets\socketio-client-ue4-example\Chat-App-Flask-SocketIO\venv
+set VIRTUAL_ENV=C:\Users\sebastian\Documents\Unreal Projects\websockets\socketio-client-ue4-example\FLASK_SocketIO\flask_websockets_soketIO\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
