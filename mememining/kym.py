1346de5651b201ac7fba0bdef055e100d5d483a0
spirch <spirch@cemm.at>
2021-11-16 18:32:12 +0100

    Update 1.txt, 1130.jpg, and 10 more files...

--- mememining/kym.py
+++ mememining/kym.py
@@ -31,58 +31,88 @@ def kymSearch(text):
 
 
 def kym_id(text):
+    exists = False
+
+    for x in nl:
+        if int(x['id']) == int(text):
+            print("already exists")
+            exists = True
+        
     """Return a meme name and url from a meme keywords.
     """
-    url = 'http://knowyourmeme.com/memes/' + str(text)
-    #print(url)
-    r = requests.get(url, headers=HEADERS)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    valid = soup.find('title')
-    print("name: " + str(valid))
+    if exists == False:
+
+        url = 'http://knowyourmeme.com/memes/' + str(text)
+        #print(url)
+        r = requests.get(url, headers=HEADERS)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        valid = soup.find('title')
+        print("name: " + str(valid))
 
 
+                
 
-    if str(valid) == '<title>Page Not Found (404) - Know Your Meme</title>':
-        print("404")
-    else:
-        x =  '{"name":"x", "id":-1, "tags":[1,2]}'
-        y = json.loads(x)
-        kids = []
-        tags = []
-        id = -1
 
-        tag_list = soup.find(class_='tags')
-        for link in tag_list.find_all('a', href=True):
-            #print(link['href'])
-            tags.append(link.get_text())
-            #
+        if str(valid) == '<title>Page Not Found (404) - Know Your Meme</title>':
+            print("404")
+        else:
+            
+
+            x =  '{"name":"x", "id":-1, "tags":[],"links":[]}'
+
+
+            #nl = []
+            #with open('nodelist.txt', 'r+') as f:
+                #nl = json.load(f)
+
+            y = json.loads(x)
+            kids = []
+            tags = []
+            #f = open("nodelist.txt", "a")
+            id = -1
+            
+            tag_list = soup.find(class_='tags')
+            for link in tag_list.find_all('a', href=True):
+                #print(link['href'])
+                tags.append(link.get_text())
+                #
+            
+
+            data = str(soup).splitlines()
+            for line in data:
+                if line.__contains__('<article class="entry" id='):
+                    id = int(re.search(r'\d+', line).group())
+                    print("ID: " + str(id))
+
+            this_url = soup.find('meta', property='og:url').get("content")
+            img_url = soup.find('meta', property='og:image').get("content")
+            #print(this_url)
+
+
+            
+            if soup.find('a', text='View Related Entries'):
+            #print(children_url)
+
+                children_url = "https://knowyourmeme.com/" + soup.find('a', text='View Related Entries').get("href")
+                kids = kym_children(children_url)
+
+            y["id"] = id
+            y["tags"] = tags
+            y["links"] = kids
+            y["name"] = valid.getText()
+            #print(nl)
+            nl.append(y)
         
+            with open('nodelist.txt', 'w') as f:
+                json.dump(nl, f)
+            #f.write(str(y))
+            #f.close()
 
-        data = str(soup).splitlines()
-        for line in data:
-            if line.__contains__('<article class="entry" id='):
-                id = int(re.search(r'\d+', line).group())
-                print("ID: " + str(id))
-
-        this_url = soup.find('meta', property='og:url').get("content")
-        img_url = soup.find('meta', property='og:image').get("content")
-        #print(this_url)
-        children_url = "https://knowyourmeme.com/" + soup.find('a', text='View Related Entries').get('href')
-        #print(children_url)
-
-
-        kids = kym_children(children_url)
-
-        y["id"] = id
-        y["tags"] = tags
-        y["links"] = kids
-        y["name"] = valid.getText()
-        print(y)
-        urllib.request.urlretrieve(img_url, str(id) + ".jpg")
-        '''with open('./{}.txt'.format(text), "w"  , encoding='utf-8') as file:
-            file.write(str(soup))
-            file.close
-        '''
+            urllib.request.urlretrieve(img_url, str(id) + ".jpg")
+            '''with open('./{}.txt'.format(text), "w"  , encoding='utf-8') as file:
+                file.write(str(soup))
+                file.close
+            '''
 def kym_children(url):
     
     #print(url)
@@ -115,5 +145,16 @@ def kym_children(url):
         return kids       # 
                 
 
+nl = []
+todo = [38850, 38601, 38577, 37024, 36009, 33967, 33893, 33702, 33081, 32889, 32416, 31855, 31779, 30877, 30634, 30332]
 
-kym_id(sys.argv[1])
+with open('nodelist.txt', 'r+') as f:
+    nl = json.load(f)
+
+print(str(len(nl)))
+print(type(nl))
+
+#for i in todo:
+for i in range(8,100):
+
+    kym_id(i)
